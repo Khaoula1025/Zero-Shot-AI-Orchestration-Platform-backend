78a99b7fde58ec7a56350d5a6171df2265408c30
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-12-07 12:51:50 +0100

    initialize analyseRouter

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI,APIRouter
 from db.database import Base,engine
-from routers.authRouter import authRouter
+from routers.authRouter import authRouter 
+from routers.analyzeRouter import analyzeRouter 
+
 app=FastAPI(
     description="Zero-Shot-Ai-Orchestration-Platform"
 )
 Base.metadata.create_all(bind=engine)
-analyzeRouter=APIRouter()
-
-
-
 
 app.include_router(authRouter)
 app.include_router(analyzeRouter)
